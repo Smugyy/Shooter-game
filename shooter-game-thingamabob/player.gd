880e3e4706570cb4976bf51d306b7189c7f7f187
Smugyy <c24492646@mytudublin.ie>
2025-05-08 11:35:09 +0100

    player movement + dash

---
+++ shooter-game-thingamabob/player.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+@export var rotation_speed = 0.08
+@export var player_speed = 500
+@export var friction = 0.5
+@export var dash_speed = 10
+
+var dash_direction = Vector2()
+var can_dash = true
+
+
+var mousePos = Vector2()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float):
+	mousePos = get_global_mouse_position()
+	var targetDir = get_angle_to(mousePos - position.normalized())
+	rotation += sin(targetDir * rotation_speed) 
+	
+	var direction = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	direction = direction.normalized()
+	velocity = velocity.lerp(direction * player_speed, 0.1)
+	velocity *= 1.0 - (friction * delta)
+	
+	if Input.is_action_just_pressed("dash") and can_dash:
+		can_dash = false
+		dash_direction = direction.normalized()
+		velocity = dash_direction * dash_speed
+		velocity *= 1.0 - (friction * delta)
+		$Dash_Cooldown.start()
+	
+	
+	move_and_collide(velocity)
+	
+	pass
+
+
+func _on_dash_cooldown_timeout() -> void:
+	can_dash = true
